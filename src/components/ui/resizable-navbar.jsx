83dde5d5f2f7f6bdcb1b7dcff1a748de8b2b8bb9
Islom <islom@Isloms-MacBook-Air.local>
2025-04-17 22:16:52 +0500

    xatolar tog'rlandi

--- src/components/ui/resizable-navbar.jsx
+++ src/components/ui/resizable-navbar.jsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { cn } from '@/lib/utils'
 import { IconMenu2, IconX } from '@tabler/icons-react'
 import {
@@ -21,22 +22,17 @@ export const Navbar = ({ children, className }) => {
 	const [visible, setVisible] = useState(false)
 
 	useMotionValueEvent(scrollY, 'change', latest => {
-		if (latest > 100) {
-			setVisible(true)
-		} else {
-			setVisible(false)
-		}
+		setVisible(latest > 100)
 	})
 
 	return (
 		<motion.div
 			ref={ref}
-			// IMPORTANT: Change this to class of `fixed` if you want the navbar to be fixed
 			className={cn('sticky inset-x-0 top-20 z-40 w-full', className)}
 		>
 			{React.Children.map(children, child =>
-				React.isValidElement(child)
-					? React.cloneElement(child, { visible })
+				React.isValidElement(child) && typeof child.type !== 'string'
+					? React.cloneElement(child, { visible }) // faqat React komponentga beriladi
 					: child
 			)}
 		</motion.div>
@@ -54,15 +50,15 @@ export const NavItems = ({ items, className, onItemClick }) => {
 				className
 			)}
 		>
-			{items.map((item, idx) => (
+			{items.map(item => (
 				<Link
-					onMouseEnter={() => setHovered(idx)}
+					key={item.name}
+					onMouseEnter={() => setHovered(item.name)}
 					onClick={onItemClick}
 					className='relative px-4 text-[16px] font-bold py-2 text-YellovCool dark:text-neutral-300 capitalize'
-					key={`link-${idx}`}
 					href={item.link}
 				>
-					{hovered === idx && (
+					{hovered === item.name && (
 						<motion.div
 							layoutId='hovered'
 							className='absolute inset-0 h-full w-full rounded-full bg-gray-100 dark:bg-neutral-800'
@@ -79,7 +75,7 @@ export const MobileNav = ({ children, className, visible }) => {
 	return (
 		<motion.div
 			animate={{
-				backdropFilter: visible ? 'blur(10px)' : 'none',
+				backdropFilter: visible ? 'blur(10px)' : undefined,
 				boxShadow: visible
 					? '0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset'
 					: 'none',
@@ -105,57 +101,50 @@ export const MobileNav = ({ children, className, visible }) => {
 	)
 }
 
-export const MobileNavHeader = ({ children, className }) => {
-	return (
-		<div
-			className={cn(
-				'flex w-full flex-row items-center justify-between',
-				className
-			)}
-		>
-			{children}
-		</div>
-	)
-}
+export const MobileNavHeader = ({ children, className }) => (
+	<div
+		className={cn(
+			'flex w-full flex-row items-center justify-between',
+			className
+		)}
+	>
+		{children}
+	</div>
+)
 
-export const MobileNavMenu = ({ children, className, isOpen, onClose }) => {
-	return (
-		<AnimatePresence>
-			{isOpen && (
-				<motion.div
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					exit={{ opacity: 0 }}
-					className={cn(
-						'absolute inset-x-0 top-16 z-50 flex w-full flex-col items-start justify-start gap-4 rounded-lg bg-white px-4 py-8 shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] dark:bg-neutral-950',
-						className
-					)}
-				>
-					{children}
-				</motion.div>
-			)}
-		</AnimatePresence>
-	)
-}
+export const MobileNavMenu = ({ children, className, isOpen }) => (
+	<AnimatePresence>
+		{isOpen && (
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				exit={{ opacity: 0 }}
+				className={cn(
+					'absolute inset-x-0 top-16 z-50 flex w-full flex-col items-start justify-start gap-4 rounded-lg bg-white px-4 py-8 shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] dark:bg-neutral-950',
+					className
+				)}
+			>
+				{children}
+			</motion.div>
+		)}
+	</AnimatePresence>
+)
 
-export const MobileNavToggle = ({ isOpen, onClick }) => {
-	return isOpen ? (
+export const MobileNavToggle = ({ isOpen, onClick }) =>
+	isOpen ? (
 		<IconX className='text-white dark:text-white' onClick={onClick} />
 	) : (
 		<IconMenu2 className='text-white dark:text-white' onClick={onClick} />
 	)
-}
 
-export const NavbarLogo = () => {
-	return (
-		<Link
-			href='#'
-			className='relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black'
-		>
-			<Image src={logos} alt='logo' width={100} height={30} />
-		</Link>
-	)
-}
+export const NavbarLogo = () => (
+	<Link
+		href='#'
+		className='relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black'
+	>
+		<Image src={logos} alt='logo' width={100} height={30} />
+	</Link>
+)
 
 export const NavbarButton = ({
 	href,
