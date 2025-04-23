46417fb9f9a5fcd5928934dd7bba9257bad29350
Islom <islom@Isloms-MacBook-Air.local>
2025-04-23 15:15:29 +0500

    kichik o'zgarish

--- src/app/[locale]/about/page.jsx
+++ src/app/[locale]/about/page.jsx
@@ -2,7 +2,7 @@ const Page = () => {
 	return (
 		<div>
 			<div className='bg-img2 min-h-[600px]'>
-				<div className='container'></div>
+				<div className='container'>hi about</div>
 			</div>
 			<div className='container'></div>
 		</div>
